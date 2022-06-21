1adb025a096c42696ca307ec7f8a9da0199ef914
kinou-p <alexpomms@gmail.com>
2022-06-21 23:53:56 +0200

    end 01 06

--- cpp01/ex06/main.cpp
+++ cpp01/ex06/main.cpp
@@ -6,8 +6,31 @@
 /*   By: apommier <apommier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/21 23:40:41 by apommier          #+#    #+#             */
-/*   Updated: 2022/06/21 23:40:55 by apommier         ###   ########.fr       */
+/*   Updated: 2022/06/21 23:52:51 by apommier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
+
+int main(int ac, char **av)
+{
+	Harl harl;
+	bool freeHarl = 0;
+	std::string	levelTab[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	
+	if (ac != 2)
+		return (0);
+	for (int j = 0; j < 4; j++)
+	{
+		if (av[1] == levelTab[j])
+			freeHarl = 1;
+		if (freeHarl == 1)
+		{
+			while (j < 4)
+			{
+				harl.complain(levelTab[j]);
+				j++;
+			}
+		}
+	}
+}
